f3e365f4b5f1564de0c5850691bdba13fb230d2d
GERLEY MACHADO DE OLIVEIRA <pb003217@pbh.rmi>
2017-08-28 11:00:25 -0300

    Dados Reais - parte 2

--- main.js
+++ main.js
@@ -5,5 +5,10 @@ fetch('/cervejarias.json')
 function setCervejarias(list){
 	const countries = list.map(c => c.country)
 	const qty = _.countBy(countries)
+	const base = _.zip(_.keys(qty), _.values(qty))
+		.sort((a, b) => a[1] > b[1] ? -1 : 1)
+		.map( a => {
+			return {country: a[0], quantity: a[1]}
+		})
 	debugger
 }
